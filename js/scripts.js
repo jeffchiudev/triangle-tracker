4a4235f22783a2caeb9d63f6262edd0b6d962aa7
Jeff Chiu <jwchiu82@gmail.com>
2020-10-22 14:06:56 -0700

    add js calculations for triangles
    co-authored-by: Patrick Osten <patrickosten040@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,19 @@ $(document).ready(function() {
         const side2 = parseInt($("input#side2").val());
         const side3 = parseInt($("input#side3").val());
 
-        if (side1 === side 2 && side2 === side3) {
-            
+        if (side1 === side2 && side2 === side3) {
+            $("#equilateral").show();
+        };
+        if (side1 === side2 && side2 !== side3) {
+            $("#isosceles").show();
+        };
+        if (side1 !== side2 && side2 !== side3 && side1 !== side3) {
+            $("#scalene").show();
+        };
+        if (side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2) {
+            $("#not-triangle").show();
         }
+
 
         event.preventDefault();
     });
