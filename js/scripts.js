897f488873e79e072dbeeb690970bc285025cf90
Jeff Chiu <jwchiu82@gmail.com>
2020-10-22 13:40:15 -0700

    add responses for triangle types
    co-authored-by: Patrick Osten <patrickosten040@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("form#triangles").submit(function(event) {
+        const side1 = parseInt($("input#side1").val());
+        const side2 = parseInt($("input#side2").val());
+        const side3 = parseInt($("input#side3").val());
+
+        if (side1 === side 2 && side2 === side3) {
+            
+        }
+
+        event.preventDefault();
+    });
+});
